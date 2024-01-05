3016a42e445cb48372aaf848c9cd889b1f01fe1d
Khushi <khu950866@gmail.com>
2024-01-05 17:58:15 +0530

    checkuser.dart

---
+++ lib/Screen/checkuser.dart
@@ -0,0 +1,28 @@
+import 'package:firebase/Screen/HomeScreen.dart';
+import 'package:firebase/Screen/signin_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class CheckUser extends StatefulWidget {
+  const CheckUser({super.key});
+
+  @override
+  State<CheckUser> createState() => _CheckUserState();
+}
+
+class _CheckUserState extends State<CheckUser> {
+  @override
+  Widget build(BuildContext context) {
+    return checkuser();
+  }
+
+  checkuser(){
+    final user=FirebaseAuth.instance.currentUser;
+    if(user!=null){
+      return HomeScreen();
+    }
+    else{
+      return SignIn();
+    }
+  }
+}
